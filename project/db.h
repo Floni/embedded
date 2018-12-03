94061a77ff18188f4720018eaa3c69c40340bc7b
Timothy Werquin <timothy@werquin.com>
2018-12-03 09:39:04 +0100

    Add comments to db.h

--- project/db.h
+++ project/db.h
@@ -9,6 +9,10 @@ using TempType = unsigned char;
 struct TempReading {
   TempType temp;
 
+  /**
+   * Print the temperature reading,
+   * the given counter is it order in the database
+   */
   void print(CounterType counter);
 };
 
@@ -19,13 +23,32 @@ struct DB {
   CounterType startCounter;
   CounterType endCounter;
 
-  void clear();
-
+  /**
+   * Initialize the database, 
+   * must be called before any operations
+   */
   void init();
 
+  /**
+   * Clear the database,
+   * earasing all content.
+   */
+  void clear();
+
+  /**
+   * Append the given temperature reading to the database
+   */
   void append(TempReading* tr);
 
+  /**
+   * Prints the latest temperature reading
+   * by calling the print method.
+   */
   void printLatest();
 
+  /**
+   * Prints all temperature readings by
+   * calling print on them.
+   */
   void printAll();
 };
